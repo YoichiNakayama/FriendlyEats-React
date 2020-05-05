33d283a4167c1a62827d58886b03ea1895c1bd1b
NakayamaYoichi <yoichi.trailheaven@gmail.com>
2020-05-05 22:52:30 +0900

    新規作成

---
+++ src/FriendlyEats/FriendlyEats.Data.js
@@ -0,0 +1,102 @@
+import * as firebase from "firebase/app"
+import "firebase/firestore";
+
+export const addRestaurant = (data) => {
+  const collection = firebase.firestore().collection('restaurants');
+  return collection.add(data);
+};
+
+export const getAllRestaurants = () => {
+  const query = firebase.firestore()
+        .collection('restaurants')
+        .orderBy('avgRating', 'desc')
+        .limit(50);
+
+  return query;
+};
+
+export const getDocumentsInQuery = (query, renderer) => {
+  return query.onSnapshot((snapshot) => {
+    if (!snapshot.size) return renderer.empty();
+
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === 'removed') {
+        renderer.remove(change.doc)
+      } else {
+        renderer.display(change.doc)
+      }
+    });
+  });
+};
+
+export const getRestaurant = (id) => {
+  return firebase.firestore().collection('restaurants').doc(id).get();
+};
+
+export const getFilteredRestaurants = (filters) => {
+  let query = firebase.firestore().collection('restaurants');
+
+  if (filters.category !== 'Any') {
+    query = query.where('category', '==', filters.category);
+  }
+
+  if (filters.city !== 'Any') {
+    query = query.where('city', '==', filters.city);
+  }
+
+  if (filters.price !== 'Any') {
+    query = query.where('price', '==', filters.price.length);
+  }
+
+  if (filters.sort === 'Rating') {
+    query = query.orderBy('avgRating', 'desc');
+  } else if (filters.sort === 'Reviews') {
+    query = query.orderBy('numRatings', 'desc');
+  }
+  return query;
+};
+
+export const addRating = (restaurantID, rating) => {
+  const collection = firebase.firestore().collection('restaurants');
+  const document = collection.doc(restaurantID);
+  const newRatingDocument = document.collection('ratings').doc();
+
+  return firebase.firestore().runTransaction(function(transaction) {
+    return transaction.get(document).then(function(doc) {
+      const data = doc.data();
+
+      const newAverage =
+            (data.numRatings * data.avgRating + rating.rating) /
+            (data.numRatings + 1);
+
+      transaction.update(document, {
+        numRatings: data.numRatings + 1,
+        avgRating: newAverage
+      });
+      return transaction.set(newRatingDocument, rating);
+    });
+  });
+};
+
+export const addWaiting = (restaurantID, waiting) => {
+  const collection = firebase.firestore().collection('restaurants');
+  const document = collection.doc(restaurantID);
+  const newWaitingDocument = document.collection('waitings').doc();
+
+  return firebase.firestore().runTransaction(function(transaction) {
+    return transaction.get(document).then(function(doc) {
+      const data = doc.data();
+      const newWaitingNo = data.waitingNo + 1;
+
+      transaction.update(document, {
+        waitingNo: newWaitingNo
+      });
+      waiting.waitingNo = newWaitingNo;      
+      return transaction.set(newWaitingDocument, waiting);
+    });
+  });
+};
+
+export const getRating = (id) => {
+  return firebase.firestore().collection('restaurants').doc(id).collection('ratings').orderBy('timestamp', 'desc');
+}
---
+++ src/Restaurant.js
@@ -0,0 +1,272 @@
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import Grid from '@material-ui/core/Grid';
+import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
+import Modal from './Modal';
+import { yellow } from '@material-ui/core/colors';
+
+import { withStyles } from '@material-ui/core/styles';
+
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+import * as FriendlyEatsData from './FriendlyEats/FriendlyEats.Data';
+import * as FriendlyEatsMock from './FriendlyEats/FriendlyEats.Mock';
+
+const styles = theme => ({
+  root: {
+    paddingTop: theme.spacing(5),
+  },
+  "guy-container": {
+    paddingTop: "100px",
+    textAlign: "center",
+  },
+  guy: {
+    maxWidth: "200px",
+    marginBottom: "20px",
+  },
+  imageHead: {
+    width: "100%",
+    height: "200px",
+    objectFit: "cover",
+  },
+  restaurantHeader: {
+    color: "white",
+    fontSize: "22px",
+    fontWeight: "bold",
+  },
+  ErrorText: {
+    color: "#000",
+  },
+  ratingStar: {
+    float: "right",
+    color: "#feb22c",
+  },
+  button: {
+    margin: "auto",
+    width: "100%",
+  },
+  restaurantHeaderItem: {
+    width: "100%",
+    "text-align": "center",
+  },
+  iconHover: {
+    float: "right",
+    margin: "0px",
+    position: "relative",
+    top: "23px",
+    "margin-right": "10px",
+    "align-self": "flex-end",
+    color: yellow[600],
+    "font-size": "46px",
+    '&:hover': {
+      color: yellow[800],
+    },
+  },
+  modalWindowHead: {
+    padding: "24px 24px 0",
+  },
+  modalWindowHeadText: {
+    float: "left",
+    fontSize: "1.25rem",
+    fontWeight: 500,
+  },
+  
+  modalWindowContent: {
+    padding: "24px 24px 0",
+    lineHeight: 1,
+  },
+  modalWindowContentStar: {
+    padding: "20px",
+  },
+  star: {
+    margin: "10px",
+    color: "#727272",
+    fontSize: "28px",
+  },
+  modalWindowContentTextarea: {
+    "width": "100%",
+    "box-sizing": "border-box",
+    "height": "100px",
+    "resize": "none",
+    "border-width": "1px 0px 1px 0px",
+    "padding": "10px",
+  },
+  modalWindowFooter: {
+    padding: "8px",
+  },
+  modalWindowFooterText: {
+    float: "right",
+  },
+});
+
+function Restaurant(props) {
+  const { classes, errorToggle } = props;
+
+  const [restaurant, setRestaurant] = useState({});
+  const [ratings, setRatings] = useState([]);
+  const [modalOpen, setModalOpen ] = useState(false);
+
+  const [rating, setRating] = useState(5);
+  const [waitingNo] = useState(0);
+  const [comment, setComment] = useState("");
+  const id = props.match.params.id;
+
+  const toggle = () => {
+    setModalOpen(!modalOpen);
+  };
+  useEffect(() => {
+    (async () => {
+      const restaurant = await FriendlyEatsData.getRestaurant(id);
+      if (restaurant && restaurant.exists) {
+        setRestaurant(restaurant.data());
+
+        const data =  await FriendlyEatsData.getRating(id);
+        const detacher = data.onSnapshot((snapshot) => {
+          const ratings = [];
+          snapshot.forEach((doc) => {
+            const rating = doc.data();
+            rating.id = doc.id;
+            ratings.push(rating);
+          });
+          setRatings(ratings);
+        });
+        return () => detacher();
+      }
+    })();
+  }, [id, ratings.length]);
+
+  const addMockRating = async (restaurantId) => {
+    try {
+      await FriendlyEatsMock.addMockRatings(restaurantId);
+    } catch (e) {
+      errorToggle("restaurant.addMockRating");
+    }
+  }
+  const myStyle = {
+    backgroundImage: "url(" + restaurant.photo + ")",
+    width: "100%",
+  }
+  const renderRating = (rating) => {
+    const ret = [];
+    for (let r = 0; r < 5; r += 1) {
+      if (r < Math.floor(rating)) {
+        ret.push(<Icon key={r}>star</Icon>);
+      } else {
+        ret.push(<Icon key={r}>star_border</Icon>);
+      }
+    }
+    return ret;
+  };
+  const randerRatingInput = () => {
+    const ret = [];
+    for (let r = 0; r < 5; r += 1) {
+      if (r < Math.floor(rating)) {
+        ret.push(<Icon key={r} onMouseOver={() => {setRating(r + 1)}} className={classes.star}>star</Icon>);
+      } else {
+        ret.push(<Icon key={r} onMouseOver={() => {setRating(r + 1)}} className={classes.star}>star_border</Icon>);
+      }
+    }
+    return ret;
+  };
+  const saveRating = async () => {
+    const ret = await FriendlyEatsData.addRating(id, {
+      rating,
+      text: comment,
+      userName: 'Anonymous (Web)',
+      timestamp: new Date(),
+      userId: firebase.auth().currentUser.uid
+    });
+    toggle();
+    if (!ret) {
+      errorToggle("restaurant.addMockRating");
+    }
+  };
+  const saveWaiting = async () => {
+    const ret = await FriendlyEatsData.addWaiting(id, {
+      waitingNo,
+      userName: 'Anonymous (Web)',
+      timestamp: new Date(),
+      userId: firebase.auth().currentUser.uid
+    });
+    toggle();
+    if (!ret) {
+      errorToggle("restaurant.addWaiting");
+    }
+  };
+  return <React.Fragment>
+    <Header />
+      <Grid container justify="center" alignItems="center" direction="column" className={classes.restaurantHeader} style={myStyle}>
+      {restaurant.name ?
+       (<React.Fragment>
+        <Grid item xs={3}/ >
+        <Grid item xs={6} className={classes.restaurantHeaderItem} >
+          <h2 style={{margin: "5px"}}>{restaurant.name}</h2>
+          <div>{renderRating(restaurant.avgRating)}</div>
+          {restaurant.city}／{restaurant.category}<br/>
+          順番待ち：{restaurant.waitingNo}組<br/>
+          <Icon className={classes.iconHover} onClick={toggle}>
+            add_circle
+          </Icon>
+        </Grid>
+        <Grid item xs={3}/ >
+        </React.Fragment>) :
+        (<div id="guy-container" className="mdc-toolbar-fixed-adjust">
+           <img className={classes.guy} src="/img/guy_fireats.png" alt="guy fireats" /><br/>
+           <div className={classes.ErrorText}>
+          No restaurant data.<br />
+          Implement getRestaurant.
+          </div>
+          <br />
+        </div>)}
+      </Grid>
+      <Grid container justify="center" alignItems="center" direction="row" className={classes.root}>
+      {(restaurant.name && ratings.length === 0) ?
+       (<div id="guy-container" className="mdc-toolbar-fixed-adjust">
+           <img className={classes.guy} src="/img/guy_fireats.png" alt="guy fireats" />
+          <div className="text">
+            This restaurant has no ratings.<br />
+          </div>
+          <br />
+        <Button color="primary" className={classes.button} onClick={() => addMockRating(id)}>Add Rating</Button>
+        </div>)
+       :
+       ratings.map((rating) => {
+         return (<React.Fragment key={rating.id}>
+                 <Grid item xs={3}/ >
+                 <Grid item xs={6} style={{ marginTop: "10px", paddingBottom: "10px", borderBottom: "1px solid"}}>
+                 <div style={{marginBottom: "8px"}}>
+                 <span style={{color: "#999"}}>{rating.userName}</span>
+                 <span className={classes.ratingStar}>{renderRating(rating.rating)}</span>
+                 </div>
+                 {rating.text}</Grid>
+                 <Grid item xs={3}/ >
+                 </React.Fragment>)
+       })
+       }
+  </Grid>
+  <Modal modalOpen={modalOpen} toggle={toggle}>
+    <div className={classes.modalWindowHead}>
+      <span className={classes.modalWindowHeadText}>Add a Review</span>
+    </div>
+    <div className={classes.modalWindowContent}>
+      <div className={classes.modalWindowContentStar}>
+        {randerRatingInput()}
+      </div>
+      <div>
+        <textarea className={classes.modalWindowContentTextarea} onChange={(e) => {setComment(e.target.value);}}></textarea>
+      </div>
+    </div>
+    <div className={classes.modalWindowFooter}>
+      <span className={classes.modalWindowFooterText}>
+        <Button color="primary" onClick={toggle}>CANCEL</Button>
+        <Button color="primary" onClick={(e ) => { saveRating()}}>SAVE</Button>
+        <Button color="primary" onClick={(e ) => { saveWaiting()}}>順番を取る</Button>
+      </span>
+    </div>
+  </Modal>
+  </React.Fragment>
+}
+
+export default withStyles(styles)(Restaurant);
